335a7f043426f623b0c4e585d1563d898f88d4d1
imldy <im.ldy@outlook.com>
2021-11-20 22:57:25 +0800

    增加在数据库中设置南区水卡号与QQ号的绑定方法

---
+++ src/main/java/cn/imldy/mriai/console/plugin/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package cn.imldy.mriai.console.plugin.mapper;
+
+import cn.imldy.mriai.console.plugin.bean.User;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author imldy
+ * @date 2021/11/20 22:28
+ **/
+@Mapper
+public interface UserMapper {
+    boolean setSouthWaterCardNoByQQId(User user);
+}
